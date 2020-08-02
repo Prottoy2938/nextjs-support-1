38a82985b4f6360cb6e15046db9b4bceb966f3aa
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-08-02 19:46:26 +0000

    QNA Successfully inside getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,24 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
+
+export async function getServerSideProps(context) {
+  const tf = require('@tensorflow/tfjs-node');
+  const qna = require('@tensorflow-models/qna');
+  async function runQna() {
+    const model = await qna.load();
+    const passage = 'Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware'
+    const question = "What's Google?"
+    // Finding the answers
+    const answers = await model.findAnswers(question, passage);
+
+    console.log('Answers: ');
+    console.log(answers);
+  }
+
+  runQna()
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}

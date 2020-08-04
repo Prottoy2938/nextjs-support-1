9918ec046dba319f35e3bafb849cb376803838b6
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-08-04 12:46:34 +0000

    Nothing to say - still working

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+
+
 export default (req, res) => {
+  if (req.method === 'POST') {
+    console.log(req)
+    console.log(req.body)
+  } else {
+    // Handle any other HTTP method
+  }
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,49 @@
 import React from "react";
 import Hello from "../src/components/hello/hello";
+import axios from "axios"
+
 
 const Home: React.FC = () => {
-  return <Hello name="Prottay" />;
+  const passage =
+    "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware";
+  const question = "What's Google?";
+
+  const handleClick = async (): void => {
+    axios.post("/api/hello", { data: "Hi there" }).then(res => {
+      console.log(res)
+    }).catch(e => {
+      console.log(e)
+    })
+  };
+
+  return (
+    <div>
+      <Hello name="Prottay" />
+      <button onClick={handleClick}>Get Result</button>
+    </div>
+  );
 };
 
 export default Home;
 
+// export async function getServerSideProps(context) {
+//   const tf = require("@tensorflow/tfjs-node");
+//   const qna = require("@tensorflow-models/qna");
+//   const model = await qna.load();
 
-export async function getServerSideProps(context) {
-  const tf = require('@tensorflow/tfjs-node');
-  const qna = require('@tensorflow-models/qna');
-  async function runQna() {
-    const model = await qna.load();
-    const passage = 'Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware'
-    const question = "What's Google?"
-    // Finding the answers
-    const answers = await model.findAnswers(question, passage);
+//   return {
+//     props: { model }, // will be passed to the page component as props
+//   };
+// }
 
-    console.log('Answers: ');
-    console.log(answers);
-  }
 
-  runQna()
-  return {
-    props: {}, // will be passed to the page component as props
-  }
-}
+// export async function getStaticProps(context) {
+//   const tf = require("@tensorflow/tfjs-node");
+//   const qna = require("@tensorflow-models/qna");
+//   let model = await qna.load();
+//   model = JSON.parse(JSON.stringify(model))
+
+//   return {
+//     props: { model }, // will be passed to the page component as props
+//   };
+// }
